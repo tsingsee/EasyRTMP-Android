f027c2e4619b12f4da22008a768e18bfd5368976
John <jiaozebo@163.com>
2018-07-07 14:58:09 +0800

    修改ui相关.添加扫码输入url的功能.(未完)

---
+++ app/src/main/java/org/easydarwin/easypusher/ScanQRActivity.java
@@ -0,0 +1,13 @@
+package org.easydarwin.easypusher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanQRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr);
+    }
+}
