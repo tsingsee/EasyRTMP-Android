e196fdaa7de8c718fc9a3c7d9323df62226317f4
liyy-23 <1290374862@qq.com>
2019-10-21 13:59:49 +0800

    添加升级功能

--- app/src/main/java/org/easydarwin/easypusher/EasyApplication.java
+++ app/src/main/java/org/easydarwin/easypusher/EasyApplication.java
@@ -6,13 +6,8 @@ import android.app.NotificationManager;
 import android.content.res.AssetManager;
 import android.os.Build;
 
-import com.squareup.otto.Bus;
-import com.squareup.otto.Subscribe;
-import com.squareup.otto.ThreadEnforcer;
-import com.tencent.bugly.crashreport.CrashReport;
-
-import org.easydarwin.bus.StartRecord;
-import org.easydarwin.bus.StopRecord;
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -38,9 +33,8 @@ public class EasyApplication extends Application {
 
         mApplication = this;
 
-        if (!BuildConfig.DEBUG) {
-            CrashReport.initCrashReport(getApplicationContext(), "678a09e90e", false);
-        }
+        Bugly.init(getApplicationContext(), "9a829a728a", false);
+        setBuglyInit();
 
         File youyuan = getFileStreamPath("SIMYOU.ttf");
         if (!youyuan.exists()) {
@@ -77,5 +71,32 @@ public class EasyApplication extends Application {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+    }
+
+    public void setBuglyInit(){
+        // 添加可显示弹窗的Activity
+        Beta.canShowUpgradeActs.add(StreamActivity.class);
+
+//        例如，只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗; 如果不设置默认所有activity都可以显示弹窗。
+//        设置是否显示消息通知
+        Beta.enableNotification = true;
+
+//        如果你不想在通知栏显示下载进度，你可以将这个接口设置为false，默认值为true。
+//        设置Wifi下自动下载
+        Beta.autoDownloadOnWifi = false;
+
+//        如果你想在Wifi网络下自动下载，可以将这个接口设置为true，默认值为false。
+//        设置是否显示弹窗中的apk信息
+        Beta.canShowApkInfo = true;
+
+//        如果你使用我们默认弹窗是会显示apk信息的，如果你不想显示可以将这个接口设置为false。
+//        关闭热更新能力
+        Beta.enableHotfix = true;
+
+        Beta.largeIconId = R.mipmap.ic_launcher_foreground;
+        Beta.smallIconId = R.mipmap.ic_launcher_foreground;
+
+        // 设置是否显示消息通知
+        Beta.enableNotification = true;
     }
 }
